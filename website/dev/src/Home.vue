844f7c5df73d25300ca6b8a95e8a6390ab35441b
userName <mario.ma@yff.com>
2021-05-30 23:37:52 +0800

    feat: 添加 website 首页导航

--- website/dev/src/Home.vue
+++ website/dev/src/Home.vue
@@ -1,9 +1,56 @@
 <template>
-	<div>Home</div>
+	<div class="page">
+		<div
+			class="comp-item"
+			v-for="comp in compList"
+			:key="comp.path"
+			@click="handleGoCompPage(comp.path)"
+		>
+			{{ comp.title }}
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+import { reactive, toRefs } from 'vue';
+import { useRouter } from 'vue-router';
+export default {
+	setup() {
+		const pageState = reactive({
+			compList: [],
+		});
+		const router = useRouter();
+		pageState.compList.push(
+			...router.options.routes
+				.map(route => {
+					if (route.name !== '/') {
+						return {
+							title: route.meta.title,
+							path: route.path,
+						};
+					}
+				})
+				.filter(item => item && item),
+		);
+		const handleGoCompPage = path => {
+			router.push(path);
+		};
+		return {
+			handleGoCompPage,
+			...toRefs(pageState),
+		};
+	},
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.page {
+	padding: 15px;
+}
+.comp-item {
+	font-size: 24px;
+	font-weight: 500;
+	padding: 15px 0;
+	border-bottom: 1px solid #eeeeee;
+}
+</style>
